0923bf0917c81277423993bc4d4f513009e48d44
QAQrz <1366138143@qq.com>
2019-05-14 10:17:59 +0800

    Initial commit

---
+++ Assets/Dev/Tile/Script/Editor/SRPGTileEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+
+namespace Maps
+{
+    [CustomEditor(typeof(SRPGTile))]
+    [CanEditMultipleObjects]
+    public class SRPGTileEditor : RuleTileEditor
+    {
+        public SRPGTile SRPGTile
+        {
+            get
+            {
+                return target as SRPGTile;
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            // 渲染新增的数据
+            EditorGUI.BeginChangeCheck();
+            SRPGTile.TerrainType = (TerrainType)EditorGUILayout.EnumPopup("地形", SRPGTile.TerrainType);
+            SRPGTile.Defense = EditorGUILayout.IntSlider("守备", SRPGTile.Defense, -100, 100);
+            SRPGTile.AvoidRate = EditorGUILayout.IntSlider("回避", SRPGTile.AvoidRate, -100, 100);
+            SRPGTile.Treatment = EditorGUILayout.IntSlider("恢复", SRPGTile.Treatment, -100, 100);
+            if (EditorGUI.EndChangeCheck())
+            {
+                EditorUtility.SetDirty(target);
+            }
+            // 渲染 RuleTile 的内容
+            EditorGUILayout.Space();
+            base.OnInspectorGUI();
+        }
+    }
+}
---
+++ Assets/Dev/Tile/Script/TerrainType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Maps
+{
+    /// <summary>
+    /// 地形类型
+    /// </summary>
+    [Serializable]
+    public enum TerrainType : byte
+    {
+        /// <summary>
+        /// 平地
+        /// </summary>
+        Plain,
+
+        /// <summary>
+        /// 森林
+        /// </summary>
+        Forest,
+
+        /// <summary>
+        /// 墙壁
+        /// </summary>
+        Wall,
+    }
+}
